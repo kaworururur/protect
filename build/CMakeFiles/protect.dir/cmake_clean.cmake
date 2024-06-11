af83c60e5de2b50f4e7f41b9fba7c627d135eb3f
Liza <elizabethbump2005@gmainl.com>
2024-06-11 09:52:47 +0300

    Initial commit

---
+++ build/CMakeFiles/protect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/protect.dir/main.cpp.o"
+  "CMakeFiles/protect.dir/main.cpp.o.d"
+  "protect"
+  "protect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/protect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/protect.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for protect.
+# This may be replaced when dependencies are built.
